76c3b50c0ca780f10be7a88d68cc99a248363093
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-07-27 19:26:09 -0400

    Footer scaffold

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,20 @@
 import React from 'react'
+import TangoLogoFB from '../images/tango-facebook-icon.svg'
+import TangoLogoIN from '../images/tango-instagram-icon.svg'
+import TangoLogoLI from '../images/tango-linkedin-icon.svg'
 
-export default () => <div>Footer</div>
+import { FooterWrapper } from './styles/FooterStyles'
+
+export default () => (
+  <>
+    <FooterWrapper>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="whiteText">CONTACT US</h1>
+          </div>
+        </div>
+      </div>
+    </FooterWrapper>
+  </>
+)
