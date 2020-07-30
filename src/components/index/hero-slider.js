0a6526a2a1242eab862b0e1c7853b0a510559e6c
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-07-30 12:27:49 -0400

    Implements a hero slider on the index page
    Using _useInterval_ from Dan Abromov at https://overreacted.io

--- src/components/index/hero-slider.js
+++ src/components/index/hero-slider.js
@@ -1,3 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { useInterval } from './useInterval'
+import Slide from './slide'
+import { SliderWrapper, ButtonWrapper, Button } from './styles/HeroSliderStyles'
 
-export default () => <div>Hero Slider</div>
+export default () => {
+  const {
+    slides: { edges: slides },
+  } = useStaticQuery(graphql`
+    {
+      slides: allWordpressWpBildspel {
+        edges {
+          node {
+            acf {
+              slider_button_link
+              slider_button_text
+              slider_header
+              slider_pre_header
+              slider_text
+            }
+            featured_media {
+              localFile {
+                childImageSharp {
+                  fluid(quality: 100, maxWidth: 4000) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+            }
+            id
+          }
+        }
+      }
+    }
+  `)
+
+  const [state, setState] = useState({
+    slides,
+    activeIndex: 0,
+    autoSlide: true,
+    interval: 7500,
+  })
+
+  // By Dan Abromov, https://overreacted.io
+  useInterval(
+    () => {
+      setState(prev => ({
+        ...prev,
+        activeIndex: (prev.activeIndex + 1) % prev.slides.length,
+      }))
+    },
+    state.autoSlide ? state.interval : null
+  )
+
+  return (
+    <SliderWrapper>
+      {slides.map((slide, i) => (
+        <Slide
+          key={slide.node.id}
+          slide={slide.node}
+          active={state.activeIndex === i}
+        />
+      ))}
+      <ButtonWrapper style={{ position: 'absolute', left: 0 }}>
+        <Button onClick={0}>&#9664;</Button>
+      </ButtonWrapper>
+      <ButtonWrapper style={{ position: 'absolute', right: 0 }}>
+        <Button onClick={0}>&#9654;</Button>
+      </ButtonWrapper>
+      {/* {JSON.stringify(state, null, 2)} */}
+    </SliderWrapper>
+  )
+  // return <pre>{JSON.stringify(slides, null, 2)}</pre>
+}
