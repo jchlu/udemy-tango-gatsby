a0b3110f23be6d0ef06d57f9916d26167dcea93b
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-02 12:20:08 -0400

    Adds the Latest Trend section

--- src/components/index/latest-trend.js
+++ src/components/index/latest-trend.js
@@ -1,3 +1,53 @@
 import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import styled from 'styled-components'
 
-export default () => <div>Latest Trend</div>
+const LatestTrendWrapper = styled.div`
+  text-align: center;
+  margin: 40px 0;
+`
+
+export default () => {
+  const {
+    data: {
+      edges: [
+        {
+          node: { excerpt, title, slug },
+        },
+      ],
+    },
+  } = useStaticQuery(graphql`
+    {
+      data: allWordpressPost(limit: 1, sort: { fields: date, order: DESC }) {
+        edges {
+          node {
+            excerpt
+            title
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <LatestTrendWrapper>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Latest Trend</h1>
+            <h4>{title}</h4>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: excerpt,
+              }}
+            />
+            <Link to={`trends/${slug}/`}>
+              <h5>Read More</h5>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </LatestTrendWrapper>
+  )
+}
