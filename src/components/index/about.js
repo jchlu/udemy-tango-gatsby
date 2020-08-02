86c7517da7624cd3ccef97646e1885c3bb3a26cd
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-02 13:28:09 -0400

    Completes the index page with the about section

--- src/components/index/about.js
+++ src/components/index/about.js
@@ -1,3 +1,55 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-export default () => <div>About</div>
+import { AboutWrapper } from './styles/AboutStyles'
+
+export default () => {
+  const {
+    about: {
+      content,
+      featured_media: {
+        localFile: {
+          childImageSharp: {
+            fluid: { src },
+          },
+        },
+      },
+    },
+  } = useStaticQuery(graphql`
+    {
+      about: wordpressPage(wordpress_id: { eq: 47 }) {
+        content
+        featured_media {
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 2000, quality: 100) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <AboutWrapper>
+      <div className="container-fluid">
+        <div className="row">
+          <div
+            className="col-md-6 aboutImage"
+            style={{
+              backgroundImage: `url(${src})`,
+            }}
+          />
+          <div className="col-md-6 aboutText">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: content,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </AboutWrapper>
+  )
+}
