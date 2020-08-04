f2774525c4c9e716bb9c4a338363cf196fa2c0f7
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-04 09:39:20 -0400

    Adds hero banner to pages

--- src/components/page-hero.js
+++ src/components/page-hero.js
@@ -1,3 +1,16 @@
 import React from 'react'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
 
-export default () => <></>
+const StyledImg = styled(Img)`
+  height: 300px;
+`
+export default ({ img }) => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col" style={{ padding: 0 }}>
+        <StyledImg fluid={img} />
+      </div>
+    </div>
+  </div>
+)
