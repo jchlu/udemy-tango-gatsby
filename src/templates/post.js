41672732b81a3e94136faf9446114fa45dbd9a3c
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-10 18:14:31 -0400

    Add placeholder PostTemplate file

---
+++ src/templates/post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => (
+  <>
+    <div className="container">
+      <div className="row">
+        <div className="col">Post Template</div>
+      </div>
+    </div>
+  </>
+)
