463dac15c66cce9dd03811536d70745d8973ca5e
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-11 15:04:42 -0400

    Working copy from the course

--- src/templates/archive.js
+++ src/templates/archive.js
@@ -1,55 +1,105 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import styled from 'styled-components'
-import Breadcrumb from '../components/breadcrumb'
+import { siteMetadata } from '../../gatsby-config'
+
 import Layout from '../components/layout'
-import PageHero from '../components/page-hero'
 import ArchiveSidebar from '../components/archive/archive-sidebar'
+import BreadCrumb from '../components/breadcrumb'
 import Pagination from '../components/archive/pagination'
+import PageHero from '../components/page-hero'
+
 import {
   PageContent,
   StyledH2,
   StyledDate,
   StyledReadMore,
 } from './styles/archiveStyles'
-import { siteMetadata } from '../../gatsby-config'
 
-const ArchiveTemplate = ({ data: { file, allWordpressPost } }) => (
+const archiveTemplate = ({
+  data: {
+    file,
+    allWordpressPost: { edges },
+  },
+  pageContext: {
+    catId,
+    catName,
+    catSlug,
+    categories,
+    humanPageNumber,
+    numberOfPages,
+  },
+}) => (
   <Layout>
     <PageHero img={file.childImageSharp.fluid} />
-    <Breadcrumb
+    <BreadCrumb
       parent={{
         link: `/trends/${siteMetadata.trendParent}`,
         title: 'trends',
       }}
     />
+
     <div className="container">
       <div className="row" style={{ marginBottom: '40px' }}>
         <ArchiveSidebar catId={catId} categories={categories} />
         <PageContent className="col-lg-9">
-          <h1 dangerouslySetInnerHTML={{ __html: catName }}></h1>
+          <Pagination
+            catSlug={catSlug}
+            page={humanPageNumber}
+            totalPages={numberOfPages}
+          />
+          <h1 dangerouslySetInnerHTML={{ __html: catName }} />
+          {edges.map(post => (
+            <article key={post.node.id} className="entry-content">
+              <Link to={`/trends/${post.node.slug}/`}>
+                <StyledH2
+                  dangerouslySetInnerHTML={{ __html: post.node.title }}
+                />
+              </Link>
+              <StyledDate
+                dangerouslySetInnerHTML={{
+                  __html: post.node.date,
+                }}
+              />
+              <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+              <StyledReadMore to={`/trends/${post.node.slug}`}>
+                Read More
+              </StyledReadMore>
+              <div className="dot_divider">
+                <hr />
+              </div>
+            </article>
+          ))}
+          <Pagination
+            catSlug={catSlug}
+            page={humanPageNumber}
+            totalPages={numberOfPages}
+          />
         </PageContent>
       </div>
     </div>
   </Layout>
 )
 
-export const query = graphql`
-  query ArchiveTemplateQuery($catId: String!, $limit: Int!, $skip: Int!) {
+export default archiveTemplate
+
+export const pageQuery = graphql`
+  query($catId: String!, $skip: Int!, $limit: Int!) {
     allWordpressPost(
       filter: { categories: { elemMatch: { id: { eq: $catId } } } }
-      limit: $limit
       skip: $skip
+      limit: $limit
     ) {
-      nodes {
-        id
-        title
-        excerpt
-        slug
-        date(formatString: "DD MMM, YYYY")
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+          date(formatString: "DD MMM YYYY")
+        }
       }
     }
-    file(absolutePath: { eq: "archive_headerImage.jpg" }) {
+    file(relativePath: { eq: "archive_headerImage.jpg" }) {
       childImageSharp {
         fluid(quality: 100, maxWidth: 4000) {
           ...GatsbyImageSharpFluid_withWebp
@@ -58,5 +108,3 @@ export const query = graphql`
     }
   }
 `
-
-export default ArchiveTemplate
