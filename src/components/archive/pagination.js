c2412649c62f16c924a6916c5f2f3de8f3bbd29e
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-11 15:42:57 -0400

    Implement Pagination component

--- src/components/archive/pagination.js
+++ src/components/archive/pagination.js
@@ -1,13 +1,38 @@
 import React from 'react'
-
-export default () => (
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import { PaginationWrapper } from './styles/PaginationStyles'
+const Pagination = ({ catSlug, page, totalPages }) => (
   <>
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div>Pagination</div>
-        </div>
-      </div>
-    </div>
+    <h2>
+      Navigation - page {page} of {totalPages}
+    </h2>
+    <PaginationWrapper>
+      {page > 1 ? (
+        <Link
+          to={`/trends/${catSlug}/${page === 2 ? '' : `${page - 1}/`}`}
+          className="navBack"
+        >
+          Previous
+        </Link>
+      ) : (
+        <div />
+      )}
+      {page < totalPages ? (
+        <Link to={`/trends/${catSlug}/${page + 1}/`} className="navForward">
+          Next
+        </Link>
+      ) : (
+        <div />
+      )}
+    </PaginationWrapper>
   </>
 )
+
+Pagination.propTypes = {
+  catSlug: PropTypes.string,
+  page: PropTypes.number,
+  totalPages: PropTypes.number,
+}
+
+export default Pagination
