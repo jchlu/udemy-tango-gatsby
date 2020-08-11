f3d1d4deed9a1bc14b127148cbca724cc2eaf6f5
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-11 12:44:46 -0400

    Initial broken archive attempt

--- src/templates/archive.js
+++ src/templates/archive.js
@@ -1,27 +1,61 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import styled from 'styled-components'
+import Breadcrumb from '../components/breadcrumb'
 import Layout from '../components/layout'
+import PageHero from '../components/page-hero'
+import ArchiveSidebar from '../components/archive/archive-sidebar'
+import Pagination from '../components/archive/pagination'
+import {
+  PageContent,
+  StyledH2,
+  StyledDate,
+  StyledReadMore,
+} from './styles/archiveStyles'
+import { siteMetadata } from '../../gatsby-config'
 
-const ArchiveTemplate = () => (
-  <>
+const ArchiveTemplate = ({ data: { file, allWordpressPost } }) => (
+  <Layout>
+    <PageHero img={file.childImageSharp.fluid} />
+    <Breadcrumb
+      parent={{
+        link: `/trends/${siteMetadata.trendParent}`,
+        title: 'trends',
+      }}
+    />
     <div className="container">
-      <div className="row">
-        <div className="col">Archive Template</div>
+      <div className="row" style={{ marginBottom: '40px' }}>
+        <ArchiveSidebar catId={catId} categories={categories} />
+        <PageContent className="col-lg-9">
+          <h1 dangerouslySetInnerHTML={{ __html: catName }}></h1>
+        </PageContent>
       </div>
     </div>
-  </>
+  </Layout>
 )
 
 export const query = graphql`
-  query ArchiveTemplateQuery(
-          $catId: Int!,
-          $catName: String!,
-          $catSlug: String!,
-          $catCount: Int!,
-          $categories: [String]!,
-        ) {
-          data: {}
+  query ArchiveTemplateQuery($catId: String!, $limit: Int!, $skip: Int!) {
+    allWordpressPost(
+      filter: { categories: { elemMatch: { id: { eq: $catId } } } }
+      limit: $limit
+      skip: $skip
+    ) {
+      nodes {
+        id
+        title
+        excerpt
+        slug
+        date(formatString: "DD MMM, YYYY")
+      }
+    }
+    file(absolutePath: { eq: "archive_headerImage.jpg" }) {
+      childImageSharp {
+        fluid(quality: 100, maxWidth: 4000) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
   }
 `
 
