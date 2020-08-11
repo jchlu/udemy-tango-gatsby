a07072abbe33a21d635c21d812458e2d1ea485d0
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-08-11 12:44:30 -0400

    Scaffold out the page elements for Archives

---
+++ src/components/archive/archive-sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+  <>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div>Archive Sidebar</div>
+        </div>
+      </div>
+    </div>
+  </>
+)
---
+++ src/components/archive/pagination.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+  <>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div>Pagination</div>
+        </div>
+      </div>
+    </div>
+  </>
+)
